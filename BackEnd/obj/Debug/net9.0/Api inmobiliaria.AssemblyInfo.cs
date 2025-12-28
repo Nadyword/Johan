9b066bf181adc51eafaddcfe1e42ae9f3b779682
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-12-28 19:37:59 -0300

    cambio de imange

--- BackEnd/obj/Debug/net9.0/Api inmobiliaria.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net9.0/Api inmobiliaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api inmobiliaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+047a5f54c3f09ded90bf3871a4051f057b62ff52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32859c868818cc08f1efa0e8a999252c61cdf7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api inmobiliaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api inmobiliaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
